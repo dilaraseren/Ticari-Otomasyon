19476555ced3e85ed0a776e5eacf68f49b93e56d
dilara153232@gmail.com <dilara153232@gmail.com>
2021-05-16 12:31:05 +0300

    16.05.2021

---
+++ Ticari Otomasyon/Controllers/GaleriController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ticari_Otomasyon.Models.Classes;
+
+namespace Ticari_Otomasyon.Controllers
+{
+    public class GaleriController : Controller
+    {
+        // GET: Galeri
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Uruns.ToList();
+            return View(degerler);
+        }
+    }
+}
