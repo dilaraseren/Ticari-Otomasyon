4a0ac0cf371b522ed3c949d220aa6e4eaac3fba3
dilara153232@gmail.com <dilara153232@gmail.com>
2021-05-29 23:25:25 +0300

    29.05.2021

---
+++ Ticari Otomasyon/Controllers/HaritaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ticari_Otomasyon.Controllers
+{
+    public class HaritaController : Controller
+    {
+        // GET: Harita
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
